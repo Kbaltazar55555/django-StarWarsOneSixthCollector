1761b217ec56b4dc8927f1198a035bdd741b9526
Kbaltazar55555 <kenneth.baltazarv@gmail.com>
2024-02-08 12:03:20 -0500

    Authentication

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import generics
+from django.http import Http404
 from .models import SW, Custom, Vehicle
 from .serializers import SWSerializer, CustomSerializer, VehicleSerializer
 
@@ -33,25 +34,37 @@ class SWDetail(generics.RetrieveUpdateDestroyAPIView):
 
 
 class CustomListCreate(generics.ListCreateAPIView):
-   serializer_class = CustomSerializer
+    serializer_class = CustomSerializer
 
-   def get_queryset(self):
-      SW_id = self.kwargs['sw_id']
-      return Custom.objects.filter(sw_id=sw_id)
+    def get_queryset(self):
+        # Ensure variable names are consistent
+        sw_id = self.kwargs['sw_id']  # Use lower case to match URL parameter
+        return Custom.objects.filter(sw_id=sw_id)
    
-   def perform_create(self, serializer):
-      SW_id = self.kwargs['sw_id']
-      SW = SW.objects.get(id=sw_id)
-      serializer.save(sw=sw)
+    def perform_create(self, serializer):
+        sw_id = self.kwargs['sw_id']  # Ensure this matches the URL parameter
+        sw = SW.objects.get(id=sw_id)  # Correct variable usage
+        serializer.save(sw=sw)
+
+
 
 class CustomDetail(generics.RetrieveUpdateDestroyAPIView):
-   queryset = Custom.objects.all()
-   serializer_class = CustomSerializer
-   lookup_field = 'id'
+    queryset = Custom.objects.all()
+    serializer_class = CustomSerializer
+    lookup_field = 'id'
 
-   def get_queryset(self):
-      SW_id = self.kwargs['SW_id']
-      return Custom.objects.filter(SW_id=SW_id)
+    def get_object(self):
+        # Directly get the object using the built-in mechanism
+        obj = super().get_object()  # This automatically uses 'id' from the URL
+
+        # Safely get 'sw_id' from the URL parameters
+        sw_id = self.kwargs.get('sw_id')
+
+        # Verify if 'sw_id' matches the Custom object's associated SW id
+        if sw_id and obj.sw.id != int(sw_id):
+            raise Http404("No Custom object found matching the SW id and Custom id.")
+
+        return obj
    
 class VehicleList(generics.ListCreateAPIView):
   queryset = Vehicle.objects.all()
