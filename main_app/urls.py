abcd6675b3f42a588d6f31c9fda022d787591ec8
Kbaltazar55555 <kenneth.baltazarv@gmail.com>
2024-02-08 13:22:52 -0500

    Authentication

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,9 +6,9 @@ urlpatterns = [
     path('sw/', SWList.as_view(), name='sw-list'), #works
     path('sw/<int:id>/', SWDetail.as_view(), name='sw-detail'), #works
     path('sw/<int:sw_id>/custom/', CustomListCreate.as_view(), name='custom-list-create'), #works
-    path('sw/<int:sw_id>/custom/<int:id>/', CustomDetail.as_view(), name='custom-detail'),
+    path('sw/<int:sw_id>/custom/<int:id>/', CustomDetail.as_view(), name='custom-detail'), #works
     path('vehicle/', VehicleList.as_view(), name='vehicle-list'), #works
     path('vehicle/<int:id>/', VehicleDetail.as_view(), name='vehicle-detail'), #works
-    path('sw/<int:sw_id>/vehicle/<int:vehicle_id>/add_vehicle/', AddVehicleToSW.as_view(), name='add-vehicle-to-sw'),
-    path('sw/<int:sw_id>/vehicle/<int:vehicle_id>/remove_vehicle/', RemoveVehicleFromSW.as_view(), name='remove-vehicle-from-sw'),
+    path('sw/<int:sw_id>/vehicle/<int:vehicle_id>/add_vehicle/', AddVehicleToSW.as_view(), name='add-vehicle-to-sw'), #works
+    path('sw/<int:sw_id>/vehicle/<int:vehicle_id>/remove_vehicle/', RemoveVehicleFromSW.as_view(), name='remove-vehicle-from-sw'), #works
 ]
